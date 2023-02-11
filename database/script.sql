4526ffc3a48d1a09deb41d9f6d9cf74d0d60115d
msirotic <martina.sirotic@student.uniri.hr>
2023-02-11 14:41:55 +0100

    modify sql script

--- database/script.sql
+++ database/script.sql
@@ -1,1 +1,1 @@
-CREATE TABLE predictions (event_id INT PRIMARY KEY AUTO_INCREMENT, request VARCHAR(100),response VARCHAR(100), time_of_request TIME, time_of_response TIME);
+CREATE TABLE predictions (event_id INT PRIMARY KEY AUTO_INCREMENT, request VARCHAR(100),response VARCHAR(100), time_of_request VARCHAR(100), time_of_response VARCHAR(100), time_elapsed INT);
